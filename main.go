361eaad13ffcd855098a4faeaf11ca2713b38bbc
Jonathan Matos <jonathan.matos2445@gmail.com>
2024-10-28 23:58:40 -0300

    feat: install gin and create routes

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"pizarria-golang/models"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	router := gin.Default()
+	router.GET("/pizzas", getPizzas)
+	router.Run()
+}
+
+func getPizzas(c *gin.Context) {
+	var pizzas = []models.Pizza{
+		{ID: 1, Nome: "Toscana", Preco: 50.2},
+		{ID: 2, Nome: "Marguerita", Preco: 90.9},
+		{ID: 3, Nome: "Atum com queijo", Preco: 100},
+	}
+	c.JSON(200, gin.H{
+		"pizzas": pizzas,
+	})
 }
